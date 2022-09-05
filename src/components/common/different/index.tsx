ef7f55b053f31ebffcb5f80755fe3ce05cc2f5ad
alpha0212 <qkrcjfgml321@naver.com>
2022-09-05 17:14:45 +0900

    Add Components: Different

---
+++ src/components/common/different/index.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import * as S from "./styled";
+
+//circle
+export const Different = () => {
+  return (
+    <>
+      <S.differentContainer>
+        <S.LeftCircleContainer>
+          <S.sCircle />
+          <S.LeftbCircle />
+        </S.LeftCircleContainer>
+        <S.textContainer>
+          <S.b>비방</S.b>
+          <S.s>과</S.s>
+          <S.b>비판</S.b>
+          <S.s>은 달라요!</S.s>
+        </S.textContainer>
+        <S.RightCircleContainer>
+          <S.RightbCircle />
+          <S.sCircle />
+        </S.RightCircleContainer>
+      </S.differentContainer>
+      <S.DescriptionContainer>
+        <S.Description>
+          비방, 비난, 그리고 비판의 사전적 의미를 살펴보자면
+        </S.Description>
+        <S.Description>
+          남을 비웃고 헐뜯어서 말하는 것, 남의 잘못이나 결점을 책잡아서 나쁘게
+          말하는 것을 비방과 비난이라고 한다.
+        </S.Description>
+        <S.Description>
+          현상이나 사물의 옳고 그름을 판단하여 밣히거나 잘못된 점을 지적하는
+          것을 비판이라고 한다.
+        </S.Description>
+        <S.Description>
+          비방, 비난과 비판은 엄연히 다르며 비판, 혹은 의견 표출이라고 하여 생각
+          표현을 지나치게 자유롭게 하다보면
+        </S.Description>
+        <S.Description>
+          자연스럽게 누군가에 대한 비방과 비난의 글이 될 수 있고 그로 인해
+          처벌을 받을 가능성이 생긱게 되므로
+        </S.Description>
+        <S.Description>
+          항상 주의하며 글을 작성하는 등의 인터넷 활동을 해야 한다.
+        </S.Description>
+      </S.DescriptionContainer>
+    </>
+  );
+};
---
+++ src/components/common/different/styled.ts
@@ -0,0 +1,74 @@
+import styled from "@emotion/styled";
+
+export const differentContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+`;
+
+export const ContainerCenter = styled.div``;
+
+export const LeftCircleContainer = styled.div`
+  margin-top: 0.4rem;
+  display: flex;
+`;
+
+export const RightCircleContainer = styled.div`
+  margin-top: 0.6rem;
+  display: flex;
+`;
+
+export const sCircle = styled.div`
+  width: 0.9rem;
+  height: 0.9rem;
+  border-radius: 20rem;
+  margin-top: 0.1rem;
+  margin-left: 2rem;
+  margin-right: 2rem;
+  background-color: #231815;
+`;
+
+export const LeftbCircle = styled.div`
+  width: 1.2rem;
+  height: 1.2rem;
+  border-radius: 20rem;
+  background-color: #231815;
+  margin-right: 7rem;
+  margin-left: 1.5rem;
+`;
+
+export const RightbCircle = styled.div`
+  width: 1.2rem;
+  height: 1.2rem;
+  border-radius: 20rem;
+  background-color: #231815;
+  margin-right: 1.5rem;
+  margin-left: 7rem;
+`;
+
+export const textContainer = styled.div`
+  letter-spacing: 0.105rem;
+  display: flex;
+`;
+
+export const b = styled.div`
+  font-size: 3rem;
+  line-height: 1.7rem;
+  font-weight: 800;
+  margin-top: 0.15rem;
+`;
+
+export const s = styled.div`
+  font-size: 2.5rem;
+  font-weight: 700;
+`;
+
+export const DescriptionContainer = styled.div`
+  justify-content: center;
+  letter-spacing: 0.15px;
+`;
+
+export const Description = styled.div`
+  text-align: center;
+  font-size: 1.5rem;
+`;
