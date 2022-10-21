075156fee3fa1ef50686b5fb0b4301f5cae8a9e3
alpha0212 <qkrcjfgml321@naver.com>
2022-10-21 18:00:22 +0900

    File: Rebuilding

---
+++ src/components/common/FixComponents/Footer/styled.ts
@@ -0,0 +1,33 @@
+import styled from "@emotion/styled";
+
+export const FooterContainer = styled.footer`
+  width: 100%;
+  display: flex;
+  padding-top: 30rem;
+  margin-bottom: 2rem;
+`;
+
+export const InfoContainer = styled.div`
+  margin-left: 37%;
+  margin-bottom: 2rem;
+`;
+
+export const Info = styled.div`
+  font-weight: 600;
+  letter-spacing: 0.55px;
+  font-size: 1.1rem;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  line-height: 1.5rem;
+`;
+
+export const InfoTextContainer = styled.div``;
+
+export const InfoText = styled.div`
+  font-weight: 600;
+  letter-spacing: 0.8px;
+  font-size: 1rem;
+  margin-top: 1rem;
+  margin-left: 5.5rem;
+  line-height: 1.5rem;
+`;
