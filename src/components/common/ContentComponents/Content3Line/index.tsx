82fe751a9a4fb2be82213f24e6f668daa9349383
alpha0212 <qkrcjfgml321@naver.com>
2022-12-21 01:06:13 +0900

    Content 3Line Create

---
+++ src/components/common/ContentComponents/Content3Line/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import * as S from "./styled";
+
+export const Content3Line: React.FC = () => {
+  return <S.ContentLine />;
+};
---
+++ src/components/common/ContentComponents/Content3Line/styled.ts
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+
+export const ContentLine = styled.div`
+  width: 100%;
+  padding-top: 20rem;
+  background: url("https://cdn.discordapp.com/attachments/1054718420651872266/1054787716274983022/Line3.png");
+  object-fit: cover;
+`;
