9cd547068c35423818bf34de1fad2b4f5f1cc694
alpha0212 <qkrcjfgml321@naver.com>
2022-12-21 18:59:31 +0900

    Change to component

---
+++ src/components/common/Category/cyberinsult/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./styled";
+
+export const CyberInsult: React.FC = () => {
+  return (
+    <S.InsultContainer>
+      <S.InsultTextContainer>
+        <S.InsultText>사이버 모욕?</S.InsultText>
+      </S.InsultTextContainer>
+    </S.InsultContainer>
+  );
+};
---
+++ src/components/common/Category/cyberinsult/styled.ts
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+export const InsultContainer = styled.div``;
+
+export const InsultTextContainer = styled.div`
+  margin-top: 13rem;
+`;
+export const InsultText = styled.div`
+  font-size: 3.5rem;
+  font-weight: bold;
+  text-align: center;
+`;
