6f72fe3340ff6fc1a0bdcd42b525bcbba3ec5329
alpha0212 <qkrcjfgml321@naver.com>
2022-12-21 19:00:24 +0900

    Defamation Page

---
+++ src/pages/ContentPages/Defamation/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import * as S from "./styled";
+import { CyberDefamation } from "src/components";
+
+export const Defamation: React.FC = () => {
+  return <CyberDefamation></CyberDefamation>;
+};
