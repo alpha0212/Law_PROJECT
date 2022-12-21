01a38090b44cab90b8f62e57ffe8b0b0c94ca728
alpha0212 <qkrcjfgml321@naver.com>
2022-12-21 18:59:18 +0900

    Change to component

---
+++ src/components/common/Category/cyberdefamation/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./styled";
+
+export const CyberDefamation: React.FC = () => {
+  return (
+    <S.DefamationContainer>
+      <S.DefamationTextContainer>
+        <S.DefamationText>사이버 명예훼손?</S.DefamationText>
+      </S.DefamationTextContainer>
+    </S.DefamationContainer>
+  );
+};
---
+++ src/components/common/Category/cyberdefamation/styled.ts
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+export const DefamationContainer = styled.div``;
+
+export const DefamationTextContainer = styled.div`
+  margin-top: 13rem;
+`;
+export const DefamationText = styled.div`
+  font-size: 3.5rem;
+  font-weight: bold;
+  text-align: center;
+`;
