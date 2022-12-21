bb034d06d05a99291aed8ede4404d521fab3760a
alpha0212 <qkrcjfgml321@naver.com>
2022-12-21 23:17:38 +0900

    Slick Slide Settings Data

--- src/pages/ContentPages/Defamation/index.tsx
+++ src/pages/ContentPages/Defamation/index.tsx
@@ -1,7 +1,32 @@
 import React from "react";
 import * as S from "./styled";
 import { CyberDefamation } from "src/components";
+import { CyberDefamationText } from "src/components/common/Category/DefamationComponents/CyberDefamationText/index";
+import { HowCyberDefamation } from "src/components/common/Category/DefamationComponents/HowCyberDefamation";
+import { DefamationCards } from "src/components/common/Category/DefamationComponents/DefamationCards";
 
 export const Defamation: React.FC = () => {
-  return <CyberDefamation></CyberDefamation>;
+  const settings = {
+    slidesToShow: 2,
+    arrows: false,
+    infinite: false,
+    variableWidth: true,
+    autoplay: false,
+    dots: false,
+    speed: 700,
+    focusOnSelect: true,
+    centerpadding: "1000px",
+  };
+  return (
+    <S.DefamationSection>
+      <S.Defamation1Line>
+        <CyberDefamation />
+        <CyberDefamationText />
+      </S.Defamation1Line>
+      <S.Defamation2Line>
+        <HowCyberDefamation />
+        <DefamationCards settings={settings} />
+      </S.Defamation2Line>
+    </S.DefamationSection>
+  );
 };
--- src/pages/ContentPages/Defamation/styled.ts
+++ src/pages/ContentPages/Defamation/styled.ts
@@ -1,1 +1,6 @@
 import styled from "@emotion/styled";
+
+export const DefamationSection = styled.div``;
+
+export const Defamation1Line = styled.div``;
+export const Defamation2Line = styled.div``;
