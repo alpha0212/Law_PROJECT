c8e269103fbee05f38ca57bb4279ea9d21cef328
alpha0212 <qkrcjfgml321@naver.com>
2022-12-27 14:26:28 +0900

    Defamation: YellowSection

---
+++ src/components/common/Category/DefamationComponents/DefamationPunishment/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import * as S from "./styled";
+
+export const DefamationPunishment: React.FC = () => {
+  return <S.YellowSection />;
+};
---
+++ src/components/common/Category/DefamationComponents/DefamationPunishment/styled.ts
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+
+export const YellowSection = styled.div`
+  width: 90rem;
+  height: 20rem;
+  border-radius: 10rem;
+  background: #ffebb7;
+`;
