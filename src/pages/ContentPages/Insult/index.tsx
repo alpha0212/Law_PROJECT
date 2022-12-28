369a4b154daaddd76c1029c444cf2991b540f866
alpha0212 <qkrcjfgml321@naver.com>
2022-12-28 21:14:37 +0900

    InsultPage 80&Fin

--- src/pages/ContentPages/Insult/index.tsx
+++ src/pages/ContentPages/Insult/index.tsx
@@ -1,7 +1,42 @@
 import React from "react";
 import * as S from "./styled";
 import { CyberInsult } from "src/components";
+import {
+  CyberInsultText,
+  InsultCards,
+} from "src/components/common/Category/InsultComponents";
+import { InsultDescription } from "src/components/common/Category/InsultComponents/InsultDescription";
+import { HowCyberInsult } from "src/components/common/Category/InsultComponents/HowCyberInsult";
+import { HowValidatedInsult } from "src/components/common/Category/InsultComponents/HowValidatedInsult";
 
 export const Insult: React.FC = () => {
-  return <CyberInsult />;
+  const settings = {
+    slidesToShow: 2,
+    arrows: false,
+    infinite: false,
+    variableWidth: true,
+    autoplay: false,
+    dots: false,
+    speed: 700,
+    focusOnSelect: true,
+    centerpadding: "1000px",
+  };
+  return (
+    <S.InsultSection>
+      <S.Insult1Line>
+        <CyberInsult />
+        <CyberInsultText />
+      </S.Insult1Line>
+      <S.Insult2Line>
+        <InsultDescription />
+      </S.Insult2Line>
+      <S.Insult3Line>
+        <HowCyberInsult />
+        <InsultCards settings={settings} />
+      </S.Insult3Line>
+      <S.Insult4Line>
+        <HowValidatedInsult />
+      </S.Insult4Line>
+    </S.InsultSection>
+  );
 };
--- src/pages/ContentPages/Insult/styled.ts
+++ src/pages/ContentPages/Insult/styled.ts
@@ -1,1 +1,8 @@
 import styled from "@emotion/styled";
+
+export const InsultSection = styled.div``;
+
+export const Insult1Line = styled.div``;
+export const Insult2Line = styled.div``;
+export const Insult3Line = styled.div``;
+export const Insult4Line = styled.div``;
