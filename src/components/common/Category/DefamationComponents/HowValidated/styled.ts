cb0ab143081a3646afb5353e043e8e79667ea82d
alpha0212 <qkrcjfgml321@naver.com>
2022-12-28 20:13:50 +0900

    Defamation: HowValidated

---
+++ src/components/common/Category/DefamationComponents/HowValidated/styled.ts
@@ -0,0 +1,52 @@
+import styled from "@emotion/styled";
+
+export const HowValidatedTitle = styled.div`
+  text-align: center;
+  font-size: 3.5rem;
+  letter-spacing: 0.35px;
+  color: #231815;
+  font-weight: 700;
+  padding-top: 27rem;
+`;
+
+export const HowValidatedCard = styled.div`
+  width: 32rem;
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+`;
+
+export const HowValidatedIcon = styled.img`
+  width: 140px;
+  height: 140px;
+  margin: auto;
+`;
+
+export const HowValidatedCardTitle = styled.div`
+  font-size: 2.5rem;
+  letter-spacing: 0.25px;
+  color: #231815;
+  text-align: center;
+  font-weight: bold;
+  padding-top: 3.5rem;
+`;
+
+export const HowValidatedCardText = styled.div`
+  text-align: center;
+  font-size: 1.5rem;
+  letter-spacing: 0.15px;
+  color: #231815;
+  line-height: 2.5rem;
+  padding-top: 1.2rem;
+`;
+
+export const HowValidatedContainer = styled.div`
+  display: flex;
+  width: 75%;
+  margin: auto;
+  padding-top: 5rem;
+`;
+
+export const HowValidatedSetContainer = styled.div`
+  width: 100%;
+`;
