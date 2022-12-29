4ab0a50dc75b1c706288a4b3dd97264a4b6332b9
alpha0212 <qkrcjfgml321@naver.com>
2022-12-29 19:53:52 +0900

    Solution: Components Routing

---
+++ src/components/common/SolutionComponents/index.ts
@@ -0,0 +1,7 @@
+export * from "./SolutionBanner";
+export * from "./SolutionOneSection";
+export * from "./SolutionTwoSection";
+export * from "./SolutionThreeSectionDescription";
+export * from "./SolutionThreeCard";
+export * from "./HorizontalLine";
+export * from "./SolutionFourSection";
