7c4545797c49c2b012038b701c121d74a07043f5
alpha0212 <qkrcjfgml321@naver.com>
2023-01-11 19:17:33 +0900

    HelpComponent: Title

---
+++ src/components/common/HelpComponents/HelpHowCanI/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import * as S from "./styled";
+
+export const HelpHowCanI: React.FC = () => {
+  return <S.HowCanITitle>무엇을 도와드릴까요?</S.HowCanITitle>;
+};
---
+++ src/components/common/HelpComponents/HelpHowCanI/styled.ts
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const HowCanITitle = styled.div`
+  font-size: 3.5rem;
+  font-weight: bold;
+  text-align: center;
+  letter-spacing: 0.3px;
+  line-height: 25px;
+  color: #231815;
+`;
