71225dd667db700919c2d30e5dbb42423db32856
alpha0212 <qkrcjfgml321@naver.com>
2023-01-11 19:18:51 +0900

    Undefined Commit

--- src/components/common/Category/DefamationComponents/DefamationPunishment/index.tsx
+++ src/components/common/Category/DefamationComponents/DefamationPunishment/index.tsx
@@ -2,5 +2,37 @@ import React from "react";
 import * as S from "./styled";
 
 export const DefamationPunishment: React.FC = () => {
-  return <S.YellowSection />;
+  return (
+    <S.YellowSection>
+      <S.DetailContainer>
+        <S.DetailPointContainer>
+          <S.DetailRedContainer>
+            <S.DetailRedPoint>
+              <S.DetailRedNumber>1</S.DetailRedNumber>
+            </S.DetailRedPoint>
+            <S.DetailRedText>정통망법</S.DetailRedText>
+          </S.DetailRedContainer>
+          <S.DetailBlueContainer>
+            <S.DetailBluePoint>
+              <S.DetailBlueNumber>2</S.DetailBlueNumber>
+            </S.DetailBluePoint>
+            <S.DetailBlueText>민법</S.DetailBlueText>
+          </S.DetailBlueContainer>
+        </S.DetailPointContainer>
+        <S.DetailTextContainer>
+          <S.DetailTextOne>
+            개인에 대한 사회적 평가를 저하시키는 표현에 대해서는
+          </S.DetailTextOne>
+          <S.DetailFlex>
+            <S.DetailTextThree>사이버 명예훼손죄</S.DetailTextThree>
+            <S.DetailTextTwo>로 처벌 받을 수 있고</S.DetailTextTwo>
+          </S.DetailFlex>
+          <S.DetailFlex style={{ marginLeft: "8.7rem" }}>
+            <S.DetailTextThree>손해배상 책임</S.DetailTextThree>
+            <S.DetailTextTwo>도 면할 수 없다.</S.DetailTextTwo>
+          </S.DetailFlex>
+        </S.DetailTextContainer>
+      </S.DetailContainer>
+    </S.YellowSection>
+  );
 };
--- src/components/common/Category/DefamationComponents/DefamationPunishment/styled.ts
+++ src/components/common/Category/DefamationComponents/DefamationPunishment/styled.ts
@@ -5,4 +5,103 @@ export const YellowSection = styled.div`
   height: 20rem;
   border-radius: 10rem;
   background: #ffebb7;
+  margin: auto;
+`;
+
+export const DetailContainer = styled.div`
+  width: 80%;
+  padding-top: 4rem;
+  display: flex;
+  margin: auto;
+`;
+
+export const DetailPointContainer = styled.div`
+  margin: auto;
+  margin-top: 3rem;
+`;
+export const DetailTextContainer = styled.div`
+  margin: auto;
+  text-align: right;
+`;
+
+export const DetailRedNumber = styled.div`
+  font-size: 2rem;
+  letter-spacing: 0.2px;
+  font-weight: bold;
+  line-height: 3.6rem;
+  text-align: center;
+  color: #ffffff;
+`;
+export const DetailBlueNumber = styled.div`
+  font-size: 2rem;
+  letter-spacing: 0.2px;
+  font-weight: bold;
+  line-height: 3.6rem;
+  text-align: center;
+  color: #ffffff;
+`;
+
+export const DetailRedContainer = styled.div`
+  display: flex;
+`;
+export const DetailRedPoint = styled.div`
+  width: 3.5rem;
+  height: 3.5rem;
+  background: #ff7878 0% 0% no-repeat padding-box;
+  border-radius: 2rem;
+`;
+export const DetailRedText = styled.div`
+  font-size: 1.5rem;
+  letter-spacing: 0.15px;
+  line-height: 2.5rem;
+  color: #ff7878;
+  margin-top: 1.4rem;
+  margin-left: 1rem;
+`;
+
+export const DetailBlueContainer = styled.div`
+  display: flex;
+  margin-top: 1.2rem;
+  margin-left: 7.7rem;
+`;
+export const DetailBluePoint = styled.div`
+  width: 3.5rem;
+  height: 3.5rem;
+  background: #919fe6 0% 0% no-repeat padding-box;
+  border-radius: 2rem;
+`;
+export const DetailBlueText = styled.div`
+  font-size: 1.5rem;
+  letter-spacing: 0.15px;
+  line-height: 2.5rem;
+  color: #919fe6;
+  margin-top: 1.55rem;
+  margin-left: 1rem;
+`;
+
+export const DetailTextOne = styled.div`
+  font-size: 1.5rem;
+  letter-spacing: 0.15px;
+  color: #231815;
+  line-height: 4rem;
+`;
+
+export const DetailTextTwo = styled.div`
+  font-size: 2rem;
+  letter-spacing: 0.2px;
+  color: #231815;
+  font-weight: 600;
+  margin-top: 1.1rem;
+`;
+export const DetailTextThree = styled.div`
+  font-size: 3rem;
+  letter-spacing: 0.3px;
+  font-weight: 600;
+  color: #231815;
+  line-height: 4rem;
+`;
+
+export const DetailFlex = styled.div`
+  display: flex;
+  text-align: right;
 `;
