18ca807726bf684b86ad698b26ecd114dcf449e3
alpha0212 <qkrcjfgml321@naver.com>
2023-01-13 19:32:06 +0900

    Routing

--- src/components/common/ContentComponents/index.ts
+++ src/components/common/ContentComponents/index.ts
@@ -1,3 +1,5 @@
 export * from "./ContentDescription";
 export * from "./Content3Line";
 export * from "./Content2Line";
+export * from "./Content4Line";
+export * from "../Category/InsultComponents/Insult5Line";
