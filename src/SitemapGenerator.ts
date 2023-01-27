e62e1db89e4e48adfea130ea712456b31aaad0f3
alpha0212 <qkrcjfgml321@naver.com>
2023-01-28 03:38:46 +0900

    🔨 Fix: Add export {}

---
+++ src/SitemapGenerator.ts
@@ -0,0 +1,15 @@
+export {};
+require("babel-register")({
+  presets: ["es2015", "react"],
+});
+
+const router = require("./SitemapRoutes.tsx").default;
+const Sitemap = require("react-router-sitemap").default;
+
+function generateSitemap() {
+  return new Sitemap(router)
+    .build("https://main.d1jnaw467bwl04.amplifyapp.com/")
+    .save("./public/sitemap.xml");
+}
+
+generateSitemap();
